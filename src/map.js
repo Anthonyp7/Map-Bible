2356d77de40c864d0817ed9f897603a8385c186c
Anthony Perozeni <anthony.perozeni777@gmail.com>
2025-07-01 12:22:56 +0200

    version stable | sidebar quasi remplie

--- src/map.js
+++ src/map.js
@@ -11,7 +11,8 @@ const map = new mapboxgl.Map({
     style: 'mapbox://styles/aperozeni7/cm5ntn1zk00d201qubbveehkn',
     center: [35.5, 25.5],
     zoom: 7,
-    maxBounds: bounds
+    maxBounds: bounds,
+    attributionControl: false // Retrait des crédits Mapbox
 });
 
 let pitch_init = 45
@@ -23,11 +24,17 @@ map.setMaxZoom(10.75);
 map.dragRotate.disable();
 map.doubleClickZoom.disable();
 
+// Ajout de l'échelle de distance
+let scale = new mapboxgl.ScaleControl()
+map.addControl(scale)
+
 
 map.easeTo({
     pitch: pitch_init,
     duration: 3000 // Durée de l'animation en millisecondes
 });
+
+
 
 
 
@@ -40,6 +47,7 @@ map.addInteraction("move-handler", {
     "layerId": "bible-dataset"
 },
     handler: (e) => {
+    // console.log("test")
     if (e.feature) {
         
         if (feature) {
@@ -71,12 +79,149 @@ map.addInteraction("leave-handler", {
 
 
 // Changement du curseur 
-map.on('mouseenter', 'bible-dataset', () => {
+map.on('mouseenter', ['bible-dataset', 'tribes'], () => {
     map.getCanvas().style.cursor = 'pointer';
 });
-map.on('mouseleave', 'bible-dataset', () => {
+map.on('mouseleave', ['bible-dataset', 'tribes'], () => {
     map.getCanvas().style.cursor = '';
 });
 
 
 
+// map.addInteraction("move-handler2", {
+//     type: "mousemove",
+//     target: {
+//     "layerId": "places"
+// },
+//     handler: (e) => {
+//     // console.log("test")
+//     if (e.feature) {
+        
+//         if (feature) {
+//         map.setFeatureState(feature, { ["state"]: false });
+//         }
+
+//         feature = e.feature;
+//         map.setFeatureState(feature, { ["state"]: true });
+//     }}
+// });
+
+
+// map.addInteraction("leave-handler2", {
+//     type: "mouseleave",
+//     target: {
+//     "layerId": "places"
+// },
+//     handler: (e) => {
+        
+//     if (feature) {
+//         map.setFeatureState(feature, { ["state"]: false });
+//         feature = null;
+//     }
+//     }
+// });
+
+
+
+
+
+
+
+function display_tribes(){
+    var layer = document.getElementById('tribes');
+    var tribes = document.querySelector('.tribes');
+    console.log("ok", layer.classList);
+    
+    if(layer.classList.contains("show")){
+        console.log("oui");
+        map.setLayoutProperty('tribes', 'visibility', 'none');
+        layer.classList.remove('show');
+        tribes.style.content = 'url("../Images/layer-light.png")';
+    }
+    else{
+        console.log("non");
+        map.setLayoutProperty('tribes', 'visibility', 'visible');
+        layer.classList.add("show");
+        tribes.style.content = 'url("../Images/layer-checked.png")';
+    }
+}
+
+
+
+function dark_mode(){
+    var toggle = document.getElementById('theme-toggle');
+    var sidebar = document.querySelector('.sidebar');
+    var botbar = document.querySelector('.botbar');
+    var h1 = document.querySelector('.botbar h1');
+    var h2 = document.querySelector('.sidebar h2');
+    var p = document.getElementById('description');
+    // var light = document.getElementById('light');
+    var passage = document.getElementById('passage');
+    console.log("ok", toggle.classList);
+
+    // LIGHT
+    if(toggle.classList.contains("show")){
+        console.log("oui");
+        map.setPaintProperty('Land - Night', 'background-opacity', 0);
+        map.setPaintProperty('hide-salt-lake night', 'fill-opacity', 0);
+        map.setPaintProperty('depth - night', 'fill-opacity', 0);
+        map.setPaintProperty('waters - night', 'fill-opacity', 0);
+        map.setPaintProperty('places', 'text-color', '#00132A');
+        map.setPaintProperty('minor-places', 'text-color', '#00132A');
+        // map.setLayoutProperty('depth - night', 'visibility', 'none');
+        // map.setLayoutProperty('waters - night', 'visibility', 'none');
+        sidebar.style.backgroundColor = 'var(--light)';
+        botbar.style.backgroundColor = 'var(--light)';
+        h1.style.color = 'var(--dark)';
+        h2.style.color = 'var(--dark)';
+        p.style.color = 'var(--dark)';
+        // light.style.color = 'var(--dark)';
+        passage.style.color = 'var(--dark)';
+        toggle.classList.remove('show');
+    }
+    // DARK
+    else{
+        console.log("non");
+        map.setPaintProperty('Land - Night', 'background-opacity', 1);
+        map.setPaintProperty('hide-salt-lake night', 'fill-opacity', 1);
+        // map.setLayoutProperty('depth - night', 'visibility', 'visible');
+        // map.setLayoutProperty('waters - night', 'visibility', 'visible');
+        map.setPaintProperty('depth - night', 'fill-opacity', 1);
+        map.setPaintProperty('waters - night', 'fill-opacity', 1);
+        map.setPaintProperty('places', 'text-color', '#FAEBD7');
+        map.setPaintProperty('minor-places', 'text-color', '#FAEBD7');
+        sidebar.style.backgroundColor = 'var(--sea)';
+        botbar.style.backgroundColor = 'var(--sea)';
+        h1.style.color = 'var(--light)';
+        h2.style.color = 'var(--light)';
+        p.style.color = 'var(--light)';
+        // light.style.color = 'var(--light)';
+        passage.style.color = 'var(--light)';
+        toggle.classList.add("show");
+    }
+}
+
+
+
+
+// map.addInteraction("move-handler3", {
+//     type: "mousemove",
+//     target: {
+//     "layerId": "places"
+// },
+//     handler: (e) => {
+//     // console.log("test")
+//     if (e.feature) {
+//         console.log(e.feature);
+//         console.log(feature);
+//         // console.log(map.getLayoutProperty('places', 'icon-image'))
+//         if (feature) {
+//             map.setPaintProperty('places', 'icon-color', '#000');
+//             // map.setFeatureState(feature, { ["state"]: false });
+//         }
+
+//         feature = e.feature;
+//         // map.setFeatureState(feature, { ["state"]: true });
+//         map.setPaintProperty('places', 'icon-color', '#fff');
+//     }}
+// });
--- src/sidebar.js
+++ src/sidebar.js
@@ -1,49 +1,150 @@
+function show_sidebar(e){
+  // Fermeture de la Botbar si elle est ouverte
+  if(botbar.classList.contains("show")){
+    hide_botbar()
+  }
+
+  var sidebar = document.getElementById('sidebar');
+    // Affichage de la sidebar
+    sidebar.classList.add('show');
+  
+    // DATA //
+    const coordinates = JSON.parse(e.feature.properties.coordinates); // JSON.parse() permet de convertir les coordonées de string à object
+    const name = e.feature.properties.name;
+    const img1 = e.feature.properties.image1;
+    const img2 = e.feature.properties.image2;
+    const description = e.feature.properties.description;
+
+
+    // AFFICHAGE DES INFORMATIONS //
+    document.getElementById('data-name').innerText = name;
+    
+    var place_type = document.querySelector('.heading');
+    if(e.feature.properties.type == "water"){
+      document.getElementById('origine').innerHTML = `
+      <div class="icon-info">
+        
+        <img class="icon" src="../Images/info.svg">
+        \n
+        <p class="light" id="light"><i>David; Jean; Paul; Jacques</i></p>
+
+        <span class="icon-info-text">étant né ou ayant vécu à cette localité</span>
+      </div>`;
+
+      place_type.style.backgroundImage = 'url("../Images/sea-banner2.png")';
+
+    }
+    else{
+      console.log("erreur ?", e.feature.properties.type);
+      place_type.style.backgroundImage = 'url("../Images/tyr1.png")';
+    }
+    document.getElementById('description').innerText = description;
+    document.getElementById('image1').innerHTML = `<img src='${img1}'>`;
+    document.getElementById('image2').innerHTML = `<img src='${img2}'>`;
+    document.getElementById('passage').innerText = "Nb 34:11; Mt 4:13; Jn 6:16-21\nLa Mer de Kinnereth, ou mer de Galilée est un lac se trouvant au nord du pays d'Israël et est traversée par le Jourdain. Plusieurs villes bordent ce lac, c'est le cas notamment de Capernaüm où Jésus à effectué un grand nombre de miracle.La Mer de Kinnereth, ou mer de Galilée est un lac se trouvant au nord du pays d'Israël et est traversée par le Jourdain. Plusieurs villes bordent ce lac, c'est le cas notamment de Capernaüm où Jésus à effectué un grand nombre de miracle.";
+
+     
+    // NAVIGATION //
+    map.easeTo({
+        center: coordinates,
+        zoom: 9,
+        // pitch: 60,
+        pitch: pitch_init,
+        duration: 2000 
+    });
+}
+
+function hide_sidebar(){
+  sidebar.classList.remove('show');
+}
+
+
+
+function show_botbar(e){
+  // Fermeture de la Sidebar si elle est ouverte
+  if(sidebar.classList.contains("show")){
+    hide_sidebar()
+  }
+  var botbar = document.getElementById('botbar');
+    botbar.classList.add('show'); // Affichage de la sidebar
+  
+    // DATA //
+    const coordinates = JSON.parse(e.feature.properties.coordinates); // JSON.parse() permet de convertir les coordonées de string à object
+    const name = e.feature.properties.name;
+    // const img1 = e.feature.properties.image1;
+    // const description = e.feature.properties.name;
+
+    // AFFICHAGE DES INFORMATIONS //
+    document.getElementById('tribe-name').innerText = name;
+    // document.getElementById('images').innerHTML = `<img src='${img1}'>`;
+
+
+    // NAVIGATION //
+    map.easeTo({
+        center: coordinates, // + 0,10 à la longitude : [35.76,32.25] --> [35.76,32.35]
+        zoom: 9,
+        // pitch: 60,
+        pitch: pitch_init,
+        duration: 2000 
+    });
+
+    // Si l'échelle est affichée
+    if(map.hasControl(scale)){
+      map.removeControl(scale)
+    }
+}
+
+function hide_botbar(){
+  botbar.classList.remove('show');
+  map.addControl(scale)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//// SIDE BAR SEAS \\\\
 map.addInteraction("Click-handler", {
     type: "click",
     target: {
       "layerId": "bible-dataset"
   },
     handler: (e) => {
-      var sidebar = document.getElementById('sidebar');
-        // console.log("Test", e.feature);//
-        // DATA //
-        const coordinates = JSON.parse(e.feature.properties.coordinates); // JSON.parse() permet de convertir les coordonées de string à object
-        const name = e.feature.properties.name;
-        // const description = e.feature.properties.name;
-
-        // AFFICHAGE DES INFORMATIONS //
-        document.getElementById('data-name').innerText = name
-
-
-        sidebar.classList.add('show'); // Affichage de la sidebar
-        
-        
-        
-
-        // NAVIGATION //
-        map.easeTo({
-            center: coordinates,
-            zoom: 9,
-            // pitch: 60,
-            pitch: pitch_init,
-            duration: 2000 
-        });
+      show_sidebar(e)
     }
 });
 
 
-// document.addEventListener("DOMContentLoaded", function() {
-//   var element = document.querySelector("#close-button");
-//   if (element) {
-//       element.addEventListener("click", function() {
-//           console.log("okay");
-//       });
-//   } else {
-//       console.error("L'élément avec l'ID 'yourElementId' n'existe pas.", element);
-//   }
-// });
+//// SIDE BAR CITIES \\\\
+map.addInteraction("Click-handler3", {
+  type: "click",
+  target: {
+    "layerId": "places"
+},
+  handler: (e) => {
+    show_sidebar(e)
+  }
+});
 
-function hide_sidebar(){
-  // alert('okokokok');
-  sidebar.classList.remove('show');
-}
+//// BOTTOM BAR \\\\
+map.addInteraction("Click-handler2", {
+  type: "click",
+  target: {
+    "layerId": "tribes"
+},
+  handler: (e) => {
+    show_botbar(e)
+  }
+});
+
+
+
+
